4ef901d30a0236d92cb85079f72fe7f1cb634057
paulocpavan <paulo.c.pavan@gmail.com>
2023-04-06 19:39:04 -0300

    alterando swiper

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  spped:600,
+  spaceBetween:300,
+ // swiper.slideNext();
   loop: true,
+  
   effect: 'cube',
+  grabCursor: true,
   cubeEffect: {
-    slideShadows: false,
+      shadow: true,
+      slideShadows: true,
+      shadowOffset:20,
+      shadowScale: 0.94,
   },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    keyboard: true,
-    
-  });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
   
