b1539cc93c67e305e631e4cb39649876b589430f
paulocpavan <paulo.c.pavan@gmail.com>
2023-04-09 22:20:59 -0300

    alteração de paginação do swipper

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  spped:600,
-  spaceBetween:300,
- // swiper.slideNext();
+  // Default parameters
   loop: true,
-  
-  effect: 'cube',
   grabCursor: true,
-  cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset:20,
-      shadowScale: 0.94,
-  },
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+    // If we need pagination
+   // pagination: {
+   //   el: '.swiper-pagination',
+   // },
   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
+
+})
