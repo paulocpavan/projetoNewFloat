3aef30c58420aa937119748afe9c8341cf1ab3e6
paulocpavan <paulo.c.pavan@gmail.com>
2023-04-11 15:00:40 -0300

    alteração de capa

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
-  // Default parameters
-  loop: true,
+  loop: true,  
   grabCursor: true,
+ 
     // If we need pagination
    // pagination: {
    //   el: '.swiper-pagination',
@@ -13,33 +13,38 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
+
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   slidesPerView: 1,
   spaceBetween: 10,
-  // Responsive breakpoints
+
   breakpoints: {
-    // when window width is >= 320px
+
     320: {
       slidesPerView: 1,
       spaceBetween: 20
     },
-    // when window width is >= 480px
+
     480: {
       slidesPerView: 2,
       spaceBetween: 30
     },
-    // when window width is >= 640px
+
     800: {
       slidesPerView: 3,
       spaceBetween: 30
     },
     1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1100: {
       slidesPerView: 4,
       spaceBetween: 30
-    }
+    },
+
   }
 
 })
